f357f0e12d5301d47107d78e3807a3ffd30815a3
Adaora Arinze <arinzea@tcd.ie>
2022-04-06 18:27:55 +0100

    added user input

--- src/Main.java
+++ src/Main.java
@@ -6,4 +6,26 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.print("Which 2 stops do you want a path between? (or enter quit) ");
+        Scanner input = new Scanner(System.in);
+        boolean quit = false;
+        while(!quit) {
+            if (input.hasNextInt()) {
+                int stops = input.nextInt();
+            }
+
+            else if (input.hasNext("quit"))
+            {
+                quit = true;
+            }
+
+            else
+            {
+                System.out.println("Error, enter a valid input or quit.");
+                input.next();
+                System.out.print("Which 2 stops do you want a path between? (or enter quit): ");
+            }
+        }
+    }
 }
