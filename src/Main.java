8e3138f3a86293ddee30d8d78b7f1c03bcf793a2
Adaora Arinze <arinzea@tcd.ie>
2022-04-08 00:11:55 +0100

    added more error checks

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,27 @@ public class Main {
         boolean quit = false;
         while(!quit) {
             if (input.hasNextInt()) {
-                int stops = input.nextInt();
+                boolean quit2 = false;
+                while(!quit2) {
+                    int firstStop = input.nextInt();
+                    System.out.print("And the second stop? (or enter quit) ");
+                    Scanner input2 = new Scanner(System.in);
+
+                    if (input.hasNextInt()) {
+                        int secondStop = input.nextInt();
+                    }
+
+                    else if (input.hasNext("quit")) {
+                        quit2 = true;
+                    }
+
+                    else {
+                        System.out.println("Error, enter a valid input or quit.");
+                        input.next();
+                        System.out.print("Which 2 stops do you want a path between? (or enter quit): ");
+                    }
+                }
+
             }
 
             else if (input.hasNext("quit"))
