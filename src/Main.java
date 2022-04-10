349fe8bc63106550b3552e03310962c67e28b682
Adaora Arinze <arinzea@tcd.ie>
2022-04-10 16:25:01 +0100

    added more user input options and error checking

--- src/Main.java
+++ src/Main.java
@@ -7,44 +7,80 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("path or stop search? (option 1 or 2?) ");
-        Scanner inputs = new Scanner(System.in);
-        if (inputs.nextInt()==1) {
-            System.out.print("Which 2 stops do you want a path between? (or enter quit) ");
-            Scanner input = new Scanner(System.in);
-            boolean quit = false;
-            while (!quit) {
-                if (input.hasNextInt()) {
-                    boolean quit2 = false;
-                    while (!quit2) {
-                        int firstStop = input.nextInt();
-                        System.out.print("And the second stop? (or enter quit) ");
-                        Scanner input2 = new Scanner(System.in);
+        boolean end = false;
+        int option=0;
+        while(!end) {
+            System.out.print("""
+                    Choose between:
+                     1) Finding the shortest path between 2 stops
+                     2) Finding a stop by searching its name
+                     3) Searching for trips by their arrival time.
+                    (type 1, 2, 3 or quit to exit the program.)\s""");
+            Scanner inputs = new Scanner(System.in);
 
+            if (inputs.hasNextInt()) {
+                option = inputs.nextInt();
+            }
+
+                if (option == 1 ) {
+                    System.out.print("Which 2 stops do you want a path between? (or enter quit) ");
+                    Scanner input = new Scanner(System.in);
+                    boolean quit = false;
+                    while (!quit) {
                         if (input.hasNextInt()) {
-                            int secondStop = input.nextInt();
-                            Sorts.TSTCreate();
+                            boolean quit2 = false;
+                            while (!quit2) {
+
+                                int firstStop = input.nextInt();
+                                System.out.print("And the second stop? (or enter quit) ");
+                                Scanner input2 = new Scanner(System.in);
+
+                                if (input.hasNextInt()) {
+                                    int secondStop = input2.nextInt();
+                                    Sorts.TSTCreate();
+                                    quit = true;
+                                }
+
+                                else if (input.hasNext("quit")) {
+                                    quit2 = true;
+                                }
+
+                                else {
+                                    System.out.println("Error, enter a valid input or quit.");
+                                    input.next();
+                                    System.out.print("Which 2 stops do you want a path between? (or enter quit): ");
+                                }
+                            }
+
                         } else if (input.hasNext("quit")) {
-                            quit2 = true;
+                            quit = true;
                         } else {
                             System.out.println("Error, enter a valid input or quit.");
                             input.next();
                             System.out.print("Which 2 stops do you want a path between? (or enter quit): ");
                         }
                     }
-
-                } else if (input.hasNext("quit")) {
-                    quit = true;
-                } else {
-                    System.out.println("Error, enter a valid input or quit.");
-                    input.next();
-                    System.out.print("Which 2 stops do you want a path between? (or enter quit): ");
+                    option = 0;
                 }
-            }
-        }
 
-        else{
-            Sorts.searchStops(".");
+                else if (option == 2) {
+                    System.out.println("okie");
+                    option = 0;
+                }
+
+                else if (option == 3) {
+                    System.out.println("ok");
+                    option = 0;
+                }
+
+                else if (inputs.hasNext("quit")) {
+                    end = true;
+                }
+
+                else {
+                    System.out.println("Error, enter a valid input or quit.");
+                    inputs.next();
+                }
         }
     }
 }
