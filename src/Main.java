8d4c0fcba7e3edb610f4a6079ee6acf440794051
Adaora Arinze <arinzea@tcd.ie>
2022-04-10 22:57:28 +0100

    removed unnecessary import statements

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,4 @@
-import java.io.FileNotFoundException;
 import java.util.*;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
